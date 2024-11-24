398ed5105ff736d7b2c0b512e28a565c63b97933
Temba <drxpnotlox@gmail.com>
2024-11-24 15:18:35 -0500

    Created v1 register UI

--- src/Components/LoginSignup/LoginSignup.jsx
+++ src/Components/LoginSignup/LoginSignup.jsx
@@ -4,24 +4,24 @@ const LoginSignup = () => {
     return (
         <div className='container'>
             <dir className='header'>
-                <div className='text'>Sign Up</div>
+                <div className='text'>neur</div>
                 <div className='underline'></div>
             </dir>
             <div className='inputs'>
                 <div className="input">
-                    <input type="username" />
+                    <input type="username" placeholder='Username'/>
                 </div>
                 <div className="input">
-                    <input type="email" />
+                    <input type="email" placeholder='Email'/>
                 </div>
                 <div className="input">
-                    <input type="password" />
+                    <input type="password" placeholder='Password' />
                 </div>
             </div>
-            <div className="forgot-password">Forgot Password? <span>Click here!</span></div>
+            <div className="forgot-password">Forgot Password?</div>
             <div className="submit-container">
-                <div className="submit">Sign Up</div>
-                <div className="submit">Login</div>
+                <div className="submitl">Login</div>
+                <div className="submits">Sign Up</div>
             </div>
         </div>
     )

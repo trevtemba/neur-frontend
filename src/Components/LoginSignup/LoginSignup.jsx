d7c3017f7e294040cbb121b768337efd3f67a0d2
Temba <drxpnotlox@gmail.com>
2024-11-24 20:25:50 -0500

    neur auth screen v1, going to refactor with proper form

--- src/Components/LoginSignup/LoginSignup.jsx
+++ src/Components/LoginSignup/LoginSignup.jsx
@@ -3,13 +3,17 @@ import './LoginSignup.css'
 import user_icon from "../Assets/user_icon.svg"
 import password_icon from "../Assets/password_icon.svg"
 import email_icon from "../Assets/email_icon.svg"
+import { useState } from 'react'
 
 const LoginSignup = () => {
+
+    const [action,setAction] = useState("Sign up");
+
     return (
         <div className='container'>
             <dir className='header'>
-                <div className='text'>neur</div>
-                <div className='underline'></div>
+                <div className='title'>neur</div>
+                <div className='slogan'>empowering entrepreneurs</div>
             </dir>
             <div className='inputs'>
                 <div className="input">
@@ -25,10 +29,10 @@ const LoginSignup = () => {
                     <input type="password" placeholder='Password' />
                 </div>
             </div>
-            <div className="forgot-password">Forgot Password?</div>
+            {action==="Login"?<div className="forgot-password">Forgot Password?</div>:<div></div>}
             <div className="submit-container">
-                <div className="submitl">Login</div>
-                <div className="submits">Sign Up</div>
+                <div className={action==="Login"?"loginLgn":"loginSu"} onClick={()=>{setAction("Submitted")}}>Create Account</div>
+                <div className={action==="Sign up"?"signupSu":"signupLgn"} onClick={()=>{setAction("Login")}}>Have an account? Login</div>
             </div>
         </div>
     )

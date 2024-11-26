ea5fa3878775ece1a75ad3d97244ae2083df926c
Temba <drxpnotlox@gmail.com>
2024-11-25 23:32:24 -0500

    Added css to new react form

--- src/Components/AuthForm/AuthForm.jsx
+++ src/Components/AuthForm/AuthForm.jsx
@@ -3,6 +3,10 @@ import FormInput from "./FormInputs/FormInput";
 import "./authForm.css"
 import React from "react";
 
+import user_icon from "../Assets/user_icon.svg"
+import email_icon from "../Assets/email_icon.svg"
+import password_icon from "../Assets/password_icon.svg"
+
 const AuthForm = () => {
 
     const [values, setValues] = useState({
@@ -18,28 +22,28 @@ const AuthForm = () => {
             name:"username",
             type:"text",
             placeholder:"Username",
-            label:"Username"
+            label: <img src={user_icon}/>
         },
         {
             id:2,
             name:"email",
             type:"text",
             placeholder:"Email",
-            label:"Email"
+            label:<img src={email_icon}/>
         },
         {
             id:3,
             name:"password",
             type:"password",
             placeholder:"Password",
-            label:"Password"
+            label:<img src={password_icon}/>
         },
         {
             id:4,
             name:"confirmPassword",
             type:"password",
             placeholder:"Confirm Password",
-            label:"Confirm Password"
+            label:<img src={password_icon}/>
         },
     ];
 

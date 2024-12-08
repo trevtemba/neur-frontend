28394e9d515a852b6a55190586a21a5aa353bd1d
Temba <drxpnotlox@gmail.com>
2024-12-07 22:37:10 -0500

    added hero and stylist featured page

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./home.css"
+import featuredHero from "../Assets/featured_hero.jpeg"
 
 const Home = () => {
     return (
@@ -9,17 +10,18 @@ const Home = () => {
 
                 <div
                 className="featuredContainer">
-                    <span className="subTitle">This Months</span>
+                    <img className="featuredHero" src={featuredHero}/>
+                    <span className="subTitle">THIS MONTH'S</span>
                     <span className="mainTitle">FEATURED</span>
+                    <span className="bottomTitle">STYLIST</span>
                     <span className="textBody">
-                        Linda Stuart has been killing it this week!
+                        <span className="personName">Linda Stuart</span> has been killing it this week!
                         Earning over 50 favorites this week, her spot on this months
                         featured is well deserved!
                     </span>
-                    <label><img src="" alt="" /></label>
                     <div className="featuredButtons">
-                        <button className="bookButtons">Book</button>
-                        <button className="favoriteButtons">Favorite</button>
+                        <button className="bookButton">BOOK</button>
+                        <button className="favoriteButton">FAVORITE</button>
                     </div>
                 </div>
 

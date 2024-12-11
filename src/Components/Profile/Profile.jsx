929a7b181379dccbe52f4e0ef19a49182e119a2a
Temba <drxpnotlox@gmail.com>
2024-12-10 23:57:12 -0500

    react framer motion and page state interaction to buttons - page interaction

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -8,42 +8,58 @@ import profileIcon from "../Assets/profileAssets/mock_icon.jpg"
 const Profile = () => {
 
     const [pageState, setPageState] = useState("info")
-    
+
+    const changePage = (buttonId) => {
+        setPageState(buttonId);
+    }
+
     return (
         <div>
-            <div className="profileHeader">
+            <motion.div className="profileHeader">
                 <div className="splash">
                     <img src={profileSplash}/>
                     <div className="icon">
                         <img src={profileIcon}/>
                         <span>username</span>
                     </div>
-                    <button className="bookBtn">
+                    <motion.button className="bookBtn">
                         BOOK
-                    </button>
-                    <button className="shareBtn">
+                    </motion.button>
+                    <motion.button className="shareBtn">
                         SHARE PROFILE
-                    </button>
+                    </motion.button>
                     <div className="reviewScore">
                         <img src=""/>
                         <span className="score">5.0</span><span className="reviewCount">(131)</span>
                     </div>
                 </div>
-            </div>
-            <div className="profileNav">
-                <button className="infoBtn">INFO</button>
-                <button className="servicesBtn">SERVICES</button>
-                <button className="reviewsBtn">REVIEWS</button>
-            </div>
-            <div className="infoPage">
-                <div></div>
-            </div>
-            <div className="servicesPage">
-                <div></div>
-            </div>
-            <div className="reviewsPage">
-                <div></div>
-            </div>   
+            </motion.div>
+            <motion.div className="profileNav">
+                <motion.button className="infoBtn" onClick={changePage("info")}>
+                    INFO
+                </motion.button>
+                <motion.button className="servicesBtn" onClick={changePage("services")}>
+                    SERVICES
+                </motion.button>
+                <motion.button className="reviewsBtn" onClick={changePage("reviews")}>
+                    REVIEWS
+                </motion.button>
+            </motion.div>
+            {pageState === "info" && (
+                <motion.div className="infoPage">
+                    <div></div>
+                </motion.div>
+            )};
+            {pageState === "services" && (
+                <motion.div className="servicesPage">
+                    <div></div>
+                </motion.div>
+            )};
+            {pageState === "reviews" && (
+                <motion.div className="reviewsPage">
+                    <div></div>
+                </motion.div>
+            )};  
         </div>
     );
 }

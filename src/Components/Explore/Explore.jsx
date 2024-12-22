550b068d7728310075d5e166b852fe71b907f1ce
Trev <92224629+trevtemba@users.noreply.github.com>
2024-12-22 18:15:09 -0500

    added animations

--- src/Components/Explore/Explore.jsx
+++ src/Components/Explore/Explore.jsx
@@ -8,7 +8,22 @@ const Explore = () => {
     const [searchState, setSearchStatee] = useState()
     return (
         <div className="explorePage">
-            <motion.div className="exploreContainer">
+            <motion.div className="exploreContainer"
+                initial={{
+                    y: -15,
+                    opacity: 0,
+                }}
+                animate={{
+                    y: 0,
+                    opacity: 1,
+                }}
+                transition={{
+                    duration: 1,
+                    ease: [0.25, 1, 0.5, 1]
+                }}
+                exit={{
+                    opacity: 0,
+                }}>
                 <form action="">
                     <label className="exploreIcon"><img src={explore_icon}/></label>
                     <input className="exploreEntry" placeholder="Search..." />
